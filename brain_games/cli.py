175aed177b62afb380e3d41f0ea7373349665ff5
Yuriy Totyshev <freewan@desktop>
2023-05-18 02:38:24 +0500

    Fix welcome message. It moved from brain_games.py to cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -3,7 +3,9 @@ import prompt
 
 def welcome_user() -> None:
     """
-    The function asks the user to enter a name in the console and prints a greeting
+    The function asks the user to enter a name
+    in the console and prints a greeting
     """
+    print("Welcome to the Brain Games!")
     name = prompt.string("May I have your name? ")
     print(f"Hello, {name}!")
