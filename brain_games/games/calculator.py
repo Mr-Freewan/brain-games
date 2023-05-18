668ec50276adb633c7a75e0d4ea4e98f775064e6
Yuriy Totyshev <freewan@desktop>
2023-05-18 22:23:02 +0500

    Add new game brain-calc

---
+++ brain_games/games/calculator.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+from random import randint, choice
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def get_question_and_answer() -> tuple[str, str]:
+    """
+    The function generates a random mathematical expression
+    (+, - or *) with two random operands and correct answer to expression.
+
+    :return: Question and correct answer to question
+    :rtype: tuple[str, str]
+    """
+    MIN_NUMBER, MAX_NUMBER = 1, 100
+    OPERATIONS = ['+', '-', '*']
+
+    operand_1 = randint(MIN_NUMBER, MAX_NUMBER)
+    operand_2 = randint(MIN_NUMBER, MAX_NUMBER)
+    operation = choice(OPERATIONS)
+
+    question = f'{operand_1} {operation} {operand_2}'
+    correct_answer = eval(question)
+
+    return question, str(correct_answer)
