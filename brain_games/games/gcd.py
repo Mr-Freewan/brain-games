802e29657794d22f6d54bb426a9409e335836ee7
Yuriy Totyshev <freewan@desktop>
2023-05-22 20:33:38 +0500

    Chore use custom gcd function instead of math.gcd()

--- brain_games/games/gcd.py
+++ brain_games/games/gcd.py
@@ -1,9 +1,30 @@
 from __future__ import annotations
 
-import math
+# import math
 import random
 
 DESCRIPTION = 'Find the greatest common divisor of given numbers.'
+
+
+def get_gcd(num_1: int, num_2: int) -> int:
+    """
+    Calculate the Greatest Common Divisor of num_1 and num_2. 
+    Euclidean algorithm used.
+
+    :param num_1: First number.
+    :type num_1: int
+    :param num_2: Second number.
+    :type num_2: int
+
+    :return: Greatest Common Divisor of num_1 and num_2
+    :rtype: int
+    """
+    num_1 = abs(num_1)
+    num_2 = abs(num_2)
+
+    while num_2 > 0:
+        num_1, num_2 = num_2, num_1 % num_2
+    return num_1
 
 
 def get_question_and_answer() -> tuple[str, str]:
@@ -22,6 +43,11 @@ def get_question_and_answer() -> tuple[str, str]:
     operand_2 = random.randint(MIN_NUMBER, MAX_NUMBER)
 
     question = f'{operand_1} {operand_2}'
-    correct_answer = math.gcd(operand_1, operand_2)
+
+    # Sumple usage, not custom
+    # correct_answer = math.gcd(operand_1, operand_2)
+
+    # Custom function usage
+    correct_answer = get_gcd(operand_1, operand_2)
 
     return question, str(correct_answer)
